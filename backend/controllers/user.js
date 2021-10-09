1b3bd177222d42648cb62936cb2454c5cee8f8e9
Michael <michaelmamaye@gmail.com>
2021-10-09 21:56:39 +0300

    hashing check

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,27 +1,34 @@
 import express from "express";
 import userModel from "../models/userModel.js";
 import {check,validationResult} from 'express-validator'
+import bcrypt from 'bcrypt'
 const router = express.Router();
 
 
 export const addNewUser = async (req,res)=>{
 
     const{ name, email, password } =req.body;
+
+    const salt= await bcrypt.genSalt(10);
+    const Password= await bcrypt.hash(password,salt);
+
     
     try{
         const checkIfExist=userModel.findOne({email})
-        if(checkIfExist){
-            res.status(400).json({ErrorEmail:'User with this email exist'})
+        console.log(checkIfExist)
+        if(checkIfExist.email===email){
+            res.status(400).json({EmailError:'User with this email already exist'})
         }
         else
         {
-        userModel.create({name,email,password})
-        res.status(200).json(req.body)
+        userModel.create({name,email,Password})
+        res.status(200).json({success:'Successfully created'})
         }
     }
     catch(err)
     {
-        res.status(400).json({msg:'add new user error',err})
+        console.log(err.message)
+        res.status(400).json({msg:'server error'})
     }
 
 }
